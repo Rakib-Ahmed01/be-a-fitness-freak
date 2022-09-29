e8cd23671961f9b2c2d11f898b7fdf7a916c2bc7
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2022-09-29 20:39:35 +0600

    questions added

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,91 +1,58 @@
 import { Accordion } from 'flowbite-react';
-import { HiOutlineArrowCircleDown } from 'react-icons/hi';
 
 const Questions = () => {
   return (
     <div>
       <div className="my-7">
-        <Accordion alwaysOpen={true} arrowIcon={HiOutlineArrowCircleDown}>
+        <Accordion alwaysOpen={true}>
           <Accordion.Panel>
-            <Accordion.Title>What is Flowbite?</Accordion.Title>
+            <Accordion.Title>How does React work?</Accordion.Title>
             <Accordion.Content>
               <p className="mb-2 text-gray-500 dark:text-gray-400">
-                Flowbite is an open-source library of interactive components
-                built on top of Tailwind CSS including buttons, dropdowns,
-                modals, navbars, and more.
-              </p>
-              <p className="text-gray-500 dark:text-gray-400">
-                Check out this guide to learn how to{' '}
-                <a
-                  href="https://flowbite.com/docs/getting-started/introduction/"
-                  className="text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  get started
-                </a>{' '}
-                and start developing websites even faster with components on top
-                of Tailwind CSS.
-              </p>
-            </Accordion.Content>
-          </Accordion.Panel>
-          <Accordion.Panel>
-            <Accordion.Title>Is there a Figma file available?</Accordion.Title>
-            <Accordion.Content>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                Flowbite is first conceptualized and designed using the Figma
-                software so everything you see in the library has a design
-                equivalent in our Figma file.
-              </p>
-              <p className="text-gray-500 dark:text-gray-400">
-                Check out the{' '}
-                <a
-                  href="https://flowbite.com/figma/"
-                  className="text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Figma design system
-                </a>{' '}
-                based on the utility classes from Tailwind CSS and components
-                from Flowbite.
+                React is a JavaScript library for building user interfaces. It
+                is a component based library. React uses a declarative paradigm
+                that makes it easier to reason about your application and aims
+                to be both efficient and flexible. It designs simple views for
+                each state in your application, and React will efficiently
+                update and render just the right component when your data
+                changes.
               </p>
             </Accordion.Content>
           </Accordion.Panel>
           <Accordion.Panel>
             <Accordion.Title>
-              What are the differences between Flowbite and Tailwind UI?
+              What is the difference between state and props?
             </Accordion.Title>
             <Accordion.Content>
               <p className="mb-2 text-gray-500 dark:text-gray-400">
-                The main difference is that the core components from Flowbite
-                are open source under the MIT license, whereas Tailwind UI is a
-                paid product. Another difference is that Flowbite relies on
-                smaller and standalone components, whereas Tailwind UI offers
-                sections of pages.
+                <code className="font-semibold">Props</code>: props is used to
+                pass data from parent to child. But we can not pass props fro
+                child t parent. We can any type of valid data as props. It is a
+                JavaScript object.
               </p>
+              <p className="text-gray-500 dark:text-gray-400">
+                <code className="font-semibold">State</code>: State is also a
+                plain Javascript object that contains information. It represents
+                the dynamic parts of the React component, i.e. data that can
+                change.
+              </p>
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>
+              What does useEffect() does other than calling the api?
+            </Accordion.Title>
+            <Accordion.Content>
               <p className="mb-2 text-gray-500 dark:text-gray-400">
-                However, we actually recommend using both Flowbite, Flowbite
-                Pro, and even Tailwind UI as there is no technical reason
-                stopping you from using the best of two worlds.
+                The useEffect Hook allows you to perform side effects (things
+                that are not in React's resposibilities) in your components.
+                useEffetct() is the combination of all life cycle methods that
+                are in Class components. Some works/function of useEffect...
               </p>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                Learn more about these technologies:
-              </p>
-              <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-                <li>
-                  <a
-                    href="https://flowbite.com/pro/"
-                    className="text-blue-600 hover:underline dark:text-blue-500"
-                  >
-                    Flowbite Pro
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://tailwindui.com/"
-                    rel="nofollow"
-                    className="text-blue-600 hover:underline dark:text-blue-500"
-                  >
-                    Tailwind UI
-                  </a>
-                </li>
+              <ul className="mb-2 text-gray-500 dark:text-gray-400 list-decimal ml-4">
+                <li>Updating DOM</li>
+                <li>Timers</li>
+                <li>etc</li>
               </ul>
             </Accordion.Content>
           </Accordion.Panel>
